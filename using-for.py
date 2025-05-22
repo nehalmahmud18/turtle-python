219a9437582971b27f3f2c9781e08ad8dacd67eb
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-05-22 08:42:17 +0600

    first commit

---
+++ using-for.py
@@ -0,0 +1,13 @@
+import turtle
+wn = turtle.Screen()
+
+elan = turtle.Turtle()
+
+distance = 50
+angle = 90
+
+for _ in range(17):
+    elan.forward(distance)
+    elan.right(angle)
+    distance = distance+ 10
+    angle = angle - 3
